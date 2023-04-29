161d7cf0654e206fbbd0452034b0f31b4412f4e2
Osvaldo Colina <osvaldo.colina@gmail.com>
2023-04-29 16:11:53 -0300

    fix: solve issue with view port and add license parameter

--- src/constants/fusioncharts.ts
+++ src/constants/fusioncharts.ts
@@ -1,12 +1,20 @@
+import Config from 'react-native-config';
+
 const FUSIONCHARTS_BASE_HTML = `
     <!doctype html>
     <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
+        <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/themes/fusioncharts.theme.fusion.js"></script>
     </head>
     <body>
         <div id="chart-container"></div>
         <script>
+            FusionCharts.options.license({
+                key: '${Config.FUSIONCHART_LICENSE_KEY}',
+                creditLabel: false,
+            });
             FusionCharts.ready(function () {
                 var chart = new FusionCharts(CHART_CONFIG).render();
             });
